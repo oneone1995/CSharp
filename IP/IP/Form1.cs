ce6acaa47d77e7870a9fcd5d81989cc5316763bd
oneone1995 <wanglixian1995@live.com>
2016-04-21 16:43:09 +0800

    fix bugs
    修复在有多个网卡情况下出现的问题

--- IP/IP/Form1.cs
+++ IP/IP/Form1.cs
@@ -1,18 +1,13 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.ComponentModel;
-using System.Data;
-using System.Drawing;
-using System.Linq;
+using System.Collections;
 using System.Net;
-using System.Text;
-using System.Threading.Tasks;
 using System.Windows.Forms;
 
 namespace IP
 {
     public partial class Form1 : Form
     {
+
         public Form1()
         {
             InitializeComponent();
@@ -22,17 +17,31 @@ namespace IP
         private void Form1_Load(object sender, EventArgs e)
         {
             IPHostEntry IpEntry = Dns.GetHostEntry(Dns.GetHostName());
+            ArrayList ipArray = new ArrayList();
             string myIp = "";
+
             foreach (IPAddress ip in IpEntry.AddressList)
             {
-                if (ip.AddressFamily == System.Net.Sockets.AddressFamily.InterNetwork)
+                if (ip.AddressFamily == System.Net.Sockets.AddressFamily.InterNetwork)//ipv4的获取
                 {
-                    //System.Diagnostics.Debug.WriteLine("LocalIPadress: " + ip);
                     myIp = ip.ToString();
+                    ipArray.Add(myIp);
+                    //break;
+                }
+            }
+
+            foreach(string resultIp in ipArray)//通过经验发现i-hdu分配的ip几乎是以10.开头，暂时没有其他办法，有更好的办法会改进
+            {
+                if(resultIp.StartsWith("10."))
+                {
+                    myIp = resultIp;
                     break;
                 }
             }
-            System.Diagnostics.Process.Start("http://192.168.2.165/portal/logon.htm?userip=" + myIp);
+            
+
+            MessageBox.Show(myIp);
+            System.Diagnostics.Process.Start("http://192.168.2.165/portal/logon.htm?userip=" + myIp);//用默认浏览器打开
             this.Close();
         }
     }
